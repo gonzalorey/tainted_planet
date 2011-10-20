cd7164fe941f50a573385851206297297cad3063
David Grandes <dgrandes@gmail.com>
2011-10-20 19:05:48 -0300

    agregado de base ship

---
+++ tainted_planet/src/scenes/base/BaseShip.h
@@ -0,0 +1,16 @@
+//
+//  BaseShip.h
+//  tainted_planet
+//
+//  Created by David Grandes on 10/15/11.
+//  Copyright 2011 OLX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface BaseShip : CCSprite {
+    
+}
+
+@end
